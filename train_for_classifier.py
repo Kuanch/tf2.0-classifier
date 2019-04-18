10b27d63862389b5ea2c56cf927b453d324adbe6
Sixigma <sixigma6@gmail.com>
2019-04-18 10:55:55 +0000

    Add cifar10 test mode

--- train_for_classifier.py
+++ train_for_classifier.py
@@ -15,6 +15,8 @@ parser = ArgumentParser()
 
 parser.add_argument('--tfrecord_path')
 
+parser.add_argument('--model_save_path')
+
 parser.add_argument('--train_image_size', type=int)
 
 parser.add_argument('--image_width', type=int)
@@ -27,14 +29,13 @@ parser.add_argument('--batch_size', type=int)
 
 parser.add_argument('--num_epoch', type=int)
 
+parser.add_argument('--cifar10_test_mode', dest='feature', action='store_true', help='if activating cifar test mode')
+parser.set_defaults(cifar10_test_mode=True)
+
 args = parser.parse_args()
 
 
-TOTAL_IMAGE_NUM = 196086
-
-
-def _total_step_one_epoch():
-  return TOTAL_IMAGE_NUM / args.batch_size
+TOTAL_IWILD_TRAIN_IMAGE = 196086
 
 
 
@@ -57,10 +58,11 @@ def train_one_step(model, optimizer, loss_fn, x_train, y_train, metrics):
 
 
 # Iterate over the batches of a dataset.
+# There is no tf.function is used here, since the incompability
+# on dataset api and some eager mode features.
+#@tf.function
 def train(model, optimizer, loss_fn, dataset, metrics, epoch):
 
-  loss = 0.0
-  accuracy = 0.0
   for e in range(epoch):
     step = 0
     for images, labels in dataset:
@@ -77,23 +79,48 @@ def train(model, optimizer, loss_fn, dataset, metrics, epoch):
 
 def main():
 
-  # Create data from iteration
-  dataset = create_dataset(args.tfrecord_path, args.batch_size)
+  if args.cifar10_test_mode:
+    dataset = create_dataset(args.tfrecord_path, args.batch_size, cifar10_test_mode=True)
   
-  # Preprocess on every images
+    train_image_size = 32
+    num_label = 10
+    test_mode = True
+
+  else:
+    dataset = create_dataset(args.tfrecord_path, args.batch_size)
+
+    train_image_size = args.train_image_size
+    num_label = args.num_label
+    test_mode = False
+
   dataset = dataset.map(lambda images, labels:preprocess_for_train(images, labels,
-                                       args.train_image_size, args.train_image_size,
-                                       args.num_label), num_parallel_calls=8)
+                                       train_image_size, train_image_size,
+                                       num_label, cifar10_test_mode=test_mode), num_parallel_calls=8)
 
-
+  # Clearing the session removes all the nodes left over
   tf.keras.backend.clear_session()
+  
+  # Learning rate config
   learning_rate_decay = tf.optimizers.schedules.ExponentialDecay(0.001, 400, 0.9)
+  
+  # Create Optimizer
   optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate_decay)
+  
+  # Cross-entorpy loss for classification
   loss_fn = tf.keras.losses.CategoricalCrossentropy()
+  
+  # Metrics for classification accuracy
   compute_accuracy = tf.keras.metrics.CategoricalAccuracy()
+  
+  # Create model
   model = Inception(args.num_label)
 
+  # Training process
   train(model, optimizer, loss_fn, dataset, compute_accuracy, args.num_epoch)
+
+  # Export the model to a SavedModel
+  tf.keras.experimental.export_saved_model(model, args.model_save_path)
+
 
 if __name__ == '__main__':
   main()
